16773dfc44ffb8e7c24e4018cafc127bbaa2284f
Yoav Cohen <98476907+CoYoav@users.noreply.github.com>
2025-01-15 19:31:06 +0200

    add: elevator subsystem with constants class

---
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems.elevator;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.excalib.control.motor.controllers.TalonFXMotor;
+import frc.excalib.mechanisms.linear_extension.LinearExtension;
+
+import java.util.function.DoubleSupplier;
+
+import static frc.robot.subsystems.elevator.Constants.*;
+
+public class Elevator extends SubsystemBase {
+    private final TalonFXMotor m_motor;
+    private final DigitalInput m_limitSwitch;
+    private final LinearExtension m_extensionMechanism;
+
+    public Elevator(){
+        m_motor = new TalonFXMotor(MOTOR_ID);
+        m_limitSwitch = new DigitalInput(LIMIT_SWITCH_CHANNEL);
+        m_extensionMechanism = new LinearExtension(
+                m_motor,
+                m_motor::getMotorPosition,
+                ()-> ELEVATOR_ANGLE,
+                ELEVATOR_GAINS,
+                UPWARD_CONSTAINTS,
+                DOWNWARD_CONSTAINTS
+        );
+    }
+    public Command manualCommand(DoubleSupplier output){
+        return m_extensionMechanism.manualCommand(output, this);
+    }
+
+
+}
