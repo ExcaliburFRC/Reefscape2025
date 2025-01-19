a06d03570d82afdc5ef49287dd2bc2489df11892
eliya1452 <eliyabl2008@gmail.com>
2025-01-19 21:52:06 +0200

    Refactor Placer class: Simplify motor initialization and remove redundant code
    - Moved motor initializations for `m_firstRotationMotor` and `m_secondRotationMotor` outside of the constructor to avoid error in motorGroup initialization.
    - Changed the method of retrieving the encoder position by using `m_angleEncoder.getPosition().getValueAsDouble()` directly in the `Arm` constructor and `m_radSupplier` and fixing the problem of it receiving degrees instead of radians.
    - Added ManualCommand to Placer; and also added goToAngle without Tolerance Consumer.

--- src/main/java/frc/robot/subsystems/arm/Placer.java
+++ src/main/java/frc/robot/subsystems/arm/Placer.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems.arm;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.spark.SparkLowLevel;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.excalib.control.math.physics.Mass;
 import frc.excalib.control.motor.controllers.MotorGroup;
@@ -11,13 +13,16 @@ import frc.excalib.control.motor.controllers.TalonFXMotor;
 import frc.excalib.mechanisms.Arm.Arm;
 import frc.excalib.mechanisms.Mechanism;
 
+import java.util.function.Consumer;
 import java.util.function.DoubleSupplier;
 
 import static frc.robot.subsystems.arm.Constants.*;
 import static java.lang.Math.*;
 
 public class Placer extends SubsystemBase {
-    private final TalonFXMotor m_firstRotationMotor, m_secondRotationMotor;
+    private final TalonFXMotor m_firstRotationMotor = new TalonFXMotor(ANGLE_MOTOR_FIRST_ID),
+            m_secondRotationMotor = new TalonFXMotor(ANGLE_MOTOR_SECOND_ID);//motorGroup...
+
     private final SparkMaxMotor m_scoringMotor, m_intakeMotor;
     private final Mechanism m_scoringWheel, m_intakeWheel;
     private final Arm m_arm;
@@ -25,9 +30,9 @@ public class Placer extends SubsystemBase {
     private final DigitalInput m_beambrake = new DigitalInput(BEAMBREAK_CHANNEL);
     private final DoubleSupplier m_radSupplier;
 
-    public Placer(){
-        m_firstRotationMotor = new TalonFXMotor(ANGLE_MOTOR_FIRST_ID);
-        m_secondRotationMotor = new TalonFXMotor(ANGLE_MOTOR_SECOND_ID);
+    private final MotorGroup motorGroup = new MotorGroup(m_firstRotationMotor, m_secondRotationMotor);
+
+    public Placer() {
 
         m_scoringMotor = new SparkMaxMotor(SHOOTER_MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
         m_intakeMotor = new SparkMaxMotor(INTAKE_MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
@@ -36,14 +41,24 @@ public class Placer extends SubsystemBase {
         m_intakeWheel = new Mechanism(m_intakeMotor);
 
         m_angleEncoder = new CANcoder(ANGLE_CANCODER_ID);
-        m_radSupplier = ()-> m_angleEncoder.getPosition().getValueAsDouble()*2 * PI;
+        m_radSupplier = () -> m_angleEncoder.getPosition().getValueAsDouble() * 2 * PI;
         m_arm = new Arm(
-                new MotorGroup(m_firstRotationMotor, m_secondRotationMotor),
-                ()-> m_angleEncoder.getPosition().getValueAsDouble(),
+                motorGroup,
+                m_radSupplier,
                 LIMIT,
-                ()-> COM_SUPPLIER,
+                () -> COM_SUPPLIER,
                 ANGLE_GAINS,
                 MASS
         );
+
     }
+
+    public Command ManualCommand(DoubleSupplier voltage) {
+        Command manualCommand = m_arm.manualCommand(voltage);
+        manualCommand.addRequirements(this);
+        return manualCommand;
+    }
+//    public Command goToAngle(DoubleSupplier angle){#TODO Add Tolerance Consumer
+//        m_arm.goToAngleCommand(angle.getAsDouble());
+//    }
 }
