14e8412701845a669ea718b0d7205bf9ab31c89f
ExcaliburFRC <excaliburfrc@gmail.com>
2025-01-23 12:14:46 +0200

    update: formated Constants

--- src/main/java/frc/robot/subsystems/arm/Constants.java
+++ src/main/java/frc/robot/subsystems/arm/Constants.java
@@ -1,37 +1,44 @@
 package frc.robot.subsystems.arm;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkFlex;
 import frc.excalib.control.gains.Gains;
 import frc.excalib.control.limits.SoftLimit;
 
 public class Constants {
+    //IDS and Channels
     public static final int ANGLE_MOTOR_FIRST_ID = 0;//TODO
     public static final int ANGLE_MOTOR_SECOND_ID = 0;//TODO
-    public static  final int INTAKE_MOTOR_ID = 0;//TODO
+    public static final int INTAKE_MOTOR_ID = 0;//TODO
     public static final int SHOOTER_MOTOR_ID = 0;//TODO
     public static final int ANGLE_CANCODER_ID = 0;//TODO
     public static final int LEFT_MOTOR_ID = 0;//TODO
-    public static final int RIGHT_MOTOR_ID=0;//TODO
+    public static final int RIGHT_MOTOR_ID = 0;//TODO
     public static final int BEAMBREAK_CHANNEL = 0;//TODO
-    public static final SoftLimit LIMIT = new SoftLimit(()->0,()->0);
-    public static final Translation2d COM_SUPPLIER = new Translation2d(0,0);
 
-    //Gains
-    private static final int kp = 0;
-    private static final int ki = 0;
-    private static final int kd = 0;
-    private static final int ka = 0;
-    private static final int kv = 0;
-    private static final int kg = 0;
-    private static final int ks = 0;
-    public static final double MASS = 0;
+    //Gains for PID and FF
+    private static final int kp = 0; // TODO
+    private static final int ki = 0; // TODO
+    private static final int kd = 0; // TODO
+    private static final int ka = 0; // TODO
+    private static final int kv = 0; // TODO
+    private static final int kg = 0; // TODO
+    private static final int ks = 0; // TODO
     public static final Gains ANGLE_GAINS = new Gains(kp, ki, kd, ks, kv, ka, kg);
-    public static final double CENTER_OF_MASS_RADIUS = 0;
-    public static final double CENTER_OF_MASS_OFFSET_RAD=0;
-    public static final double MAX_OFFSET = 0.2;
 
-    //VELS
-    public static final double ALGAE_REMOVAL_OUTPUT = 0; //TODO
+
+    //Physical Proprieties
+    public static final Translation2d COM_SUPPLIER = new Translation2d(0, 0);
+    public static final double MASS = 0; // TODO
+    public static final double CENTER_OF_MASS_RADIUS = 0; // TODO
+    public static final double CENTER_OF_MASS_OFFSET_RAD = 0; // TODO
+    public static final double MAX_OFFSET = 0; // TODO
+    private static final double MIN_LIMIT = 0.0; // TODO
+    private static final double MAX_LIMIT = 0.0; // TODO
+    public static final SoftLimit LIMIT = new SoftLimit(() -> MIN_LIMIT, () -> MAX_LIMIT);
+
+    //Velocities and DutyCycles
+    public static final double ALGAE_REMOVAL_DC = 0; //TODO
     public static final double INTAKE_CORAL_DC = 0; //TODO
     public static final double OUTPUT_CORAL_DC = 0; //TODO
 
