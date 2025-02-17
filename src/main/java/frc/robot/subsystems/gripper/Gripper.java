bac2982f856664c7be3f16eb3128c7cd159371e4
Keller6738 <itaykeller12345@gmail.com>
2025-02-17 10:28:58 +0200

    fix: sort Gripper subsystem class

--- src/main/java/frc/robot/subsystems/gripper/Gripper.java
+++ src/main/java/frc/robot/subsystems/gripper/Gripper.java
@@ -1,42 +1,40 @@
 package frc.robot.subsystems.gripper;
 
 import com.revrobotics.ColorSensorV3;
-import com.revrobotics.spark.SparkLowLevel;
-import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj2.command.*;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.excalib.control.motor.controllers.FlexMotor;
 import frc.excalib.control.motor.controllers.TalonFXMotor;
 import frc.excalib.mechanisms.Mechanism;
-import frc.robot.superstructure.State;
-import monologue.Annotations;
 import monologue.Logged;
 
-import java.util.function.DoubleSupplier;
-
+import static com.revrobotics.spark.SparkLowLevel.MotorType.kBrushless;
+import static edu.wpi.first.wpilibj.I2C.Port.kOnboard;
 import static frc.robot.subsystems.gripper.Constants.*;
-import static monologue.Annotations.*;
+import static monologue.Annotations.Log;
 
 public class Gripper extends SubsystemBase implements Logged {
     private final FlexMotor m_outerMotor;
     private final TalonFXMotor m_innerMotor;
     private final Mechanism m_outerWheel, m_innerWheel;
-    private final ColorSensorV3 m_colorSensor = new ColorSensorV3(I2C.Port.kOnboard);
+    private final ColorSensorV3 m_colorSensor = new ColorSensorV3(kOnboard);
     public final Trigger m_coralTrigger = new Trigger(() -> m_colorSensor.getProximity() > PROXIMITY_LIMIT);
 
     public Gripper() {
-        m_outerMotor = new FlexMotor(OUTER_MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
+        m_outerMotor = new FlexMotor(OUTER_MOTOR_ID, kBrushless);
         m_innerMotor = new TalonFXMotor(INNER_MOTOR_ID);
         m_outerWheel = new Mechanism(m_outerMotor);
         m_innerWheel = new Mechanism(m_innerMotor);
-
     }
 
     public Command manualCommand(double innerVoltage, double outerVoltage) {
         Command manualCommand = new ParallelCommandGroup(
                 new RunCommand(() -> m_innerWheel.setVoltage(innerVoltage)),
                 new RunCommand(() -> m_outerWheel.setVoltage(outerVoltage))
-        );
+        ).withName("Manual Command");
         manualCommand.addRequirements(this);
         return manualCommand;
     }
