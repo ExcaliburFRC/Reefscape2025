55ac34789b859e41c1bbe9f18263460178ee54c9
keller6738 <itaykeller12345@gmail.com>
2025-03-05 12:02:38 +0200

    add: constants file and sort AlgaeSystem class

--- src/main/java/frc/robot/subsystems/algae/AlgaeSystem.java
+++ src/main/java/frc/robot/subsystems/algae/AlgaeSystem.java
@@ -7,30 +7,46 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.excalib.control.motor.controllers.TalonFXMotor;
 import frc.excalib.mechanisms.Mechanism;
+import monologue.Annotations;
+
+import static frc.robot.subsystems.algae.Constants.*;
+import static monologue.Annotations.*;
 
 public class AlgaeSystem extends SubsystemBase {
-    private final Trigger m_hasAlgae;
+    private final Trigger m_hasAlgaeTrigger;
     private final Mechanism m_algaeWheel;
-    private double voltageState;
+    private double m_voltageState;
 
     public AlgaeSystem() {
-        this.m_algaeWheel = new Mechanism(new TalonFXMotor(0));
-        this.m_hasAlgae = new Trigger(() -> this.m_algaeWheel.logCurrent() > 0).debounce(0);
-       this.voltageState = 0;
+        this.m_algaeWheel = new Mechanism(new TalonFXMotor(MOTOR_ID));
+
+        this.m_hasAlgaeTrigger = new Trigger(
+                () -> this.m_algaeWheel.logCurrent() > HAS_ALGAE_CURRENT
+        ).debounce(HAS_ALGAE_DEBOUNCE);
+
+        this.m_voltageState = 0;
         this.setDefaultCommand(defaultCommand());
     }
 
+    public Command manualCommand(double voltage) {
+        return m_algaeWheel.manualCommand(() -> voltage, this);
+    }
 
     private Command defaultCommand() {
         return new RunCommand(
                 () -> {
-                    this.m_algaeWheel.setVoltage(this.voltageState);
+                    this.m_algaeWheel.setVoltage(this.m_voltageState);
                 },
                 this
         );
     }
 
     public Command setStateCommand(double voltageState) {
-        return new InstantCommand(() -> this.voltageState = voltageState);
+        return new RunCommand(() -> this.m_voltageState = voltageState).until(() -> m_voltageState == voltageState);
+    }
+
+    @Log.NT
+    public boolean hasAlgae() {
+        return m_hasAlgaeTrigger.getAsBoolean();
     }
 }
---
+++ src/main/java/frc/robot/subsystems/algae/Constants.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.algae;
+
+public class Constants {
+    public static final int MOTOR_ID = 0; //TODO
+    public static final double HAS_ALGAE_CURRENT = 0; //TODO
+    public static final double HAS_ALGAE_DEBOUNCE = 0; //TODO
+}
