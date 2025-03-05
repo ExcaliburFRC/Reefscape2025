5fc7888d56c9b8965f581500aa744e0a527c378c
keller6738 <itaykeller12345@gmail.com>
2025-03-05 12:02:24 +0200

    add: constants file and sort CoralSystem class

---
+++ src/main/java/frc/robot/subsystems/coral/Constants.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.coral;
+
+public class Constants {
+    public static final int MOTOR_ID = 0; //TODO
+    public static final int PROXIMITY_LIMIT = 80;
+    public static final double HAS_CORAL_CURRENT = 0; //TODO
+    public static final double HAS_CORAL_DEBOUNCE = 0; //TODO
+}
--- src/main/java/frc/robot/subsystems/coral/CoralSystem.java
+++ src/main/java/frc/robot/subsystems/coral/CoralSystem.java
@@ -9,34 +9,49 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.excalib.control.motor.controllers.TalonFXMotor;
 import frc.excalib.mechanisms.Mechanism;
+import monologue.Annotations;
+
+import static edu.wpi.first.wpilibj.I2C.Port.kOnboard;
+import static frc.robot.subsystems.coral.Constants.*;
+import static monologue.Annotations.*;
 
 public class CoralSystem extends SubsystemBase {
-    private final Trigger m_hasCoral;
+    private final Trigger m_hasCoralTrigger;
     private final Mechanism m_coralWheel;
-    private double voltageState;
     private final ColorSensorV3 m_colorSensor;
+    private double m_voltageState;
 
     public CoralSystem() {
-        this.m_colorSensor = new ColorSensorV3(I2C.Port.kOnboard);
-        this.m_coralWheel = new Mechanism(new TalonFXMotor(0));
-        this.m_hasCoral = new Trigger(
-                () -> this.m_coralWheel.logCurrent() > 0 ||
-                        this.m_colorSensor.getProximity() > 80
-        ).debounce(0);
-        this.voltageState = 0;
+        this.m_colorSensor = new ColorSensorV3(kOnboard);
+
+        this.m_coralWheel = new Mechanism(new TalonFXMotor(MOTOR_ID));
+
+        this.m_hasCoralTrigger = new Trigger(
+                () -> (this.m_coralWheel.logCurrent() > HAS_CORAL_CURRENT) ||
+                        (this.m_colorSensor.getProximity() > PROXIMITY_LIMIT)
+        ).debounce(HAS_CORAL_DEBOUNCE);
+
+        this.m_voltageState = 0;
         this.setDefaultCommand(defaultCommand());
+    }
+
+    public Command manualCommand(double voltage) {
+        return m_coralWheel.manualCommand(() -> voltage, this).withName("Manual Command");
     }
 
     private Command defaultCommand() {
         return new RunCommand(
-                () -> {
-                    this.m_coralWheel.setVoltage(this.voltageState);
-                },
+                () -> this.m_coralWheel.setVoltage(this.m_voltageState),
                 this
-        );
+        ).withName("Default Command");
     }
 
     public Command setStateCommand(double voltageState) {
-        return new InstantCommand(() -> this.voltageState = voltageState);
+        return new RunCommand(() -> this.m_voltageState = voltageState).until(() -> m_voltageState == voltageState);
+    }
+
+    @Log.NT
+    public boolean hasCoral() {
+        return m_hasCoralTrigger.getAsBoolean();
     }
 }
