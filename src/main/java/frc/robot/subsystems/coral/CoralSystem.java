da72a97fe603cfb850261e147876134cbceb81a4
Yehuda Rothstein <yehudarothstein@gmail.com>
2025-03-10 20:16:09 +0200

    feat: option for bumper switch

--- src/main/java/frc/robot/subsystems/coral/CoralSystem.java
+++ src/main/java/frc/robot/subsystems/coral/CoralSystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.coral;
 
 import com.revrobotics.ColorSensorV3;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -19,18 +20,19 @@ import static monologue.Annotations.*;
 
 public class CoralSystem extends SubsystemBase implements Logged {
     public final Trigger m_hasCoralTrigger;
+    public final DigitalInput m_bumperSwitch = new DigitalInput(BUMPER_SWITCH_ID);
     boolean hasCoral = false;
     private final Mechanism m_coralWheel;
     private double m_voltageState;
 
     public CoralSystem() {
-
         this.m_coralWheel = new Mechanism(new TalonFXMotor(MOTOR_ID));
 
         this.m_hasCoralTrigger = new Trigger(
                 () -> hasCoral
         ).debounce(HAS_CORAL_DEBOUNCE);
 
+//        this.m_hasCoralTrigger = new Trigger(()-> m_bumperSwitch.get()).debounce(HAS_CORAL_DEBOUNCE);
         this.m_voltageState = 0;
         this.setDefaultCommand(defaultCommand());
     }
