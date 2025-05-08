d6a41baba2801768519d38efee4ca54b10ad5bf2
Yehuda Rothstein <yehudarothstein@gmail.com>
2025-05-08 14:36:20 +0300

    fix: basic RobotContainer layout

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,6 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -12,7 +11,6 @@ import monologue.Monologue;
 
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
-
     private final RobotContainer m_robotContainer;
 
     /**
@@ -20,9 +18,6 @@ public class Robot extends TimedRobot {
      * initialization code.
      */
     public Robot() {
-
-        // Instantiate our RobotContainer.
-        // This will perform all our button bindings, and put our autonomous chooser on the dashboard.
         m_robotContainer = new RobotContainer();
     }
 
@@ -65,8 +60,7 @@ public class Robot extends TimedRobot {
     public void autonomousInit() {
         m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
-        // schedule the autonomous command (example)6w
-        if (m_autonomousCommand != null) {
+        if (m_autonomousCommand != null){
             m_autonomousCommand.schedule();
         }
     }
@@ -94,7 +88,6 @@ public class Robot extends TimedRobot {
 
     @Override
     public void testInit() {
-        // Cancels all running commands at the start of test mode.
         CommandScheduler.getInstance().cancelAll();
     }
 
